a8db3d537bb6f78faf09b8fbf185667c9bbd3412
Ninila <ninila_company@bk.ru>
2021-07-30 20:54:31 +0500

    to compile apk file

--- main.py
+++ main.py
@@ -13,32 +13,36 @@ class MainApp(App):
     def build(self):
         main_layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
-        self.solution1 = TextInput(multiline=False, readonly=False, halign='left', font_size=20,
-                                   hint_text='Введите начальный диаметр роля', write_tab=False,)
-        self.solution2 = TextInput(multiline=False, readonly=False, halign='left', font_size=20,
-                                   hint_text='Введите начальную массу роля', write_tab=False)
-        self.solution3 = TextInput(multiline=False, readonly=False, halign='left', font_size=20,
-                                   hint_text='Введите диаметр остатка', write_tab=False)
+        self.solution1 = TextInput(multiline=False, readonly=False, halign='left', font_size=55,
+                                   hint_text='Введите начальный диаметр роля', write_tab=False,
+                                   input_filter='float')
+        self.solution2 = TextInput(multiline=False, readonly=False, halign='left', font_size=55,
+                                   hint_text='Введите начальную массу роля', write_tab=False,
+                                   input_filter='float')
+        self.solution3 = TextInput(multiline=False, readonly=False, halign='left', font_size=55,
+                                   hint_text='Введите диаметр остатка', write_tab=False,
+                                   input_filter='float')
         self.print_result = Label()
         main_layout.add_widget(self.solution1)
         main_layout.add_widget(self.solution2)
         main_layout.add_widget(self.solution3)
         main_layout.add_widget(self.print_result)
 
-        buttons = [
-            ['7', '8', '9'],
-            ['4', '5', '6'],
-            ['1', '2', '3'],
-            ['.', '0', 'C']
-        ]
-
-        for row in buttons:
-            h_layout = BoxLayout()
-            for label in row:
-                button = Button(text=label, pos_hint={'center_x': 0.5, 'center_y': 0.5})
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
+        # КЛАВИАТУРА ДЛЯ ПРИЛОЖЕНИЯ (ПОКА УБРАЛ)
+        # buttons = [
+        #     ['7', '8', '9'],
+        #     ['4', '5', '6'],
+        #     ['1', '2', '3'],
+        #     ['.', '0', 'C']
+        # ]
+        #
+        # for row in buttons:
+        #     h_layout = BoxLayout()
+        #     for label in row:
+        #         button = Button(text=label, pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        #         button.bind(on_press=self.on_button_press)
+        #         h_layout.add_widget(button)
+        #     main_layout.add_widget(h_layout)
 
         equals_button = Button(text='РЕЗУЛЬТАТ', pos_hint={'center_x': 0.5, 'center_y': 0.5})
         equals_button.bind(on_press=self.on_solution)
@@ -52,9 +56,9 @@ class MainApp(App):
         else:
             if self.solution1.focus:
                 self.solution1.text += instance.text
-            if self.solution2.focus:
+            elif self.solution2.focus:
                 self.solution2.text += instance.text
-            if self.solution3.focus:
+            else:
                 self.solution3.text += instance.text
 
     def on_solution(self, instance):
